d918cc8a18064bcfd0f85fcdb57b82f1cedead81
Jonathan Petrone <44399523+JonathanPetrone@users.noreply.github.com>
2023-10-03 21:48:12 +0200

    another

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,13 +1,20 @@
 const chai = require('chai');
 let assert = chai.assert;
 const ConvertHandler = require('../controllers/convertHandler.js');
-
-let convertHandler = new ConvertHandler();
+const convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
 
-    test('#isNull, #isNotNull', function () {
-        assert.isNull(null, 'This is an optional error description - e.g. null is null');
-        assert.isNotNull(1, '1 is not null');
+    test('First test', function () {
+        
+        // Arrange
+        const input = "1kg";
+        
+        // Act: Call the getNum method with the test input
+        const num = ConvertHandler.getNum(input);
+        
+        // Assert: Compare the actual result to the expected result
+        const expectedNum = 1;
+        assert.equal(num, expectedNum, `Expected ${expectedNum}, but got ${num}`);
       });
 });
